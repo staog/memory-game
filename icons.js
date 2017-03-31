4a650579b451e50f7be9b461bc73ac09484e2826
Milan V. Kecojević <staog@gmx.com>
2017-03-31 21:32:46 +0200

    Add icons

---
+++ icons.js
@@ -0,0 +1,11 @@
+var icons = ['fa-address-book','fa-adjust','fa-bars','fa-calculator','fa-car',
+'fa-bus','fa-bomb','fa-beer','fa-clone','fa-cog','fa-coffee','fa-database',
+'fa-cubes','fa-flask','fa-gavel','fa-glass','fa-bed','fa-random','fa-times',
+'fa-tint','fa-trash','fa-truck','fa-users','fa-user','fa-taxi','fa-ship',
+'fa-motorcycle','fa-neuter','fa-eur','fa-usd','fa-blind','fa-flag',
+'fa-address-book','fa-adjust','fa-bars','fa-calculator','fa-car','fa-bus',
+'fa-bomb','fa-beer','fa-clone','fa-cog','fa-coffee','fa-database','fa-cubes',
+'fa-flask','fa-gavel','fa-glass','fa-bed','fa-random','fa-times','fa-tint',
+'fa-trash','fa-truck','fa-users','fa-user','fa-taxi','fa-ship','fa-motorcycle',
+'fa-neuter','fa-eur','fa-usd','fa-blind','fa-flag'];
+console.log(icons.length);
